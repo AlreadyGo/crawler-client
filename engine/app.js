57a7dfe6e260a327ae785e67fe674d91b99d8574
zhouhui <18862245093@163.com>
2018-01-27 16:49:31 +0800

    multi page support

--- engine/app.js
+++ engine/app.js
@@ -1,33 +1,37 @@
-import Executor from './executor';
-import WebPage from './webpage';
+import Executor from "./executor";
+import WebPage from "./webpage";
+import WebPagePool from "./webPagePool";
+
 require('file-loader?name=index.html!./index.html');
 
-$toggleCrawlerLog();
+window.WebPagePool = WebPagePool;
 
 window.runConfig = (config, args = {}, order = 0) => {
-  if (args.debug === undefined) args.debug = false;
-  return Executor.run($views[order], config, args);
+    if (args.debug === undefined) args.debug = false;
+    return Executor.run($views[order], config, args);
 };
-window.resetWebpage = ( orders = 1) => {
-  if (window.$views && window.$views.length>0) $views.forEach($view=>{$view.dispose();});
-  window.$views = [];
-  for(var i=0;i<orders;i++){
-      $views.push(new WebPage(`${Math.random()}`))
-  }
-  // $view.requestUrl("https://www.taobao.com/");
+window.resetWebpage = (numbers = 1) => {
+    if (window.$views && window.$views.length > 0) {
+        $views.forEach($view => {
+            $view.dispose();
+        });
+    }
+
+    window.$views = (new Array(numbers)).fill(0).map((v,i) => (new WebPage(i)));
+    window.$views.forEach($view => {$view.requestUrl("https://www.taobao.com/");})
 };
 
-window.showDevTool = ( order = 1 ) => $views[order].webview.openDevTools();
+window.showDevTool = (order = 0) => $views[order].webview.openDevTools();
 
-window.load = (url, order = 1) => $views[order].requestUrl(url);
+window.load = (url, order = 0) => $views[order].requestUrl(url);
 
 $(() => {
-  console.log("####################################");
-  console.log("Welcome to the unicrawler playground.");
-  console.log("Use `load(url)` to open url in the target webview if you need to login before running a config.");
-  console.log("Use `showDevTool()` to open the dev tool of the target webview.");
-  console.log("Use `runConfig(config, args)` to test.");
-  console.log("Use `resetWebpage()` to reset this tool, cache and cookies will deleted.");
-  console.log("####################################");
-  resetWebpage();
+    console.log("####################################");
+    console.log("Welcome to the unicrawler playground.");
+    console.log("Use `load(url)` to open url in the target webview if you need to login before running a config.");
+    console.log("Use `showDevTool()` to open the dev tool of the target webview.");
+    console.log("Use `runConfig(config, args)` to test.");
+    console.log("Use `resetWebpage()` to reset this tool, cache and cookies will deleted.");
+    console.log("####################################");
+    resetWebpage();
 });
---
+++ engine/webPagePool.js
@@ -0,0 +1,75 @@
+import WebPage from "./webpage";
+import Executor from "./executor";
+
+let runConfigByView = ($view, config, args = {}) => {
+    return Executor.run($view, config, args);
+}
+
+function WebPagePool(number = 1) {
+    const queue = [];
+    let failQueue = [];
+    let toStop = false;
+    let $views = (new Array(number)).fill(0).map((v, i) => (new WebPage(`${Math.random()}`)));
+    let check = (condition, callback) => {
+        let c;
+        if ((c = condition())) {
+            callback(c);
+        }
+        setTimeout(() => {
+            check(condition, callback);
+        }, 100);
+
+    }
+    this.submit = (config, args, callback) => {
+        queue.push({config, args, callback});
+    }
+
+    this.close = () => {
+        toStop = true;
+    }
+
+    this.isStop = () => toStop;
+
+    this.failBack = () => {
+        queue.push(...failQueue)
+        failQueue = [];
+    };
+
+    this.failQueue = () => failQueue;
+
+    this.isOver = () => (queue.length === 0);
+    let endlessLoop = () => {
+        check(
+            () => {
+                return !toStop
+            },
+            () => {
+                if (queue.length > 0) {
+                    check(() => {
+                            let $view;
+                            return ($view = $views.shift());
+                        },
+                        ($view) => {
+                            let task = queue.shift();
+                            if (task) {
+                                const {config, args, callback} = task;
+                                runConfigByView($view, config, args)
+                                    .then(v => {
+                                        callback(v);
+                                        $views.unshift($view);
+                                    }).catch(e => {
+                                    console.error(e);
+                                    failQueue.push(task);
+                                    $views.unshift($view);
+                                })
+                            }
+                        }
+                    )
+                }
+            }
+        )
+    }
+    endlessLoop();
+}
+
+export default WebPagePool;
